9fc260e75bbf5831f3987f90bc719af4448ec600
JJakiminski <49916964+JJakiminski@users.noreply.github.com>
2019-07-31 02:36:25 +0200

    Update ImgEditor.py

--- ImgEditor.py
+++ ImgEditor.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror
 import PIL
@@ -13,21 +13,35 @@ global file_path
 
 
 def get_image():
+    global file_path
     file_path = filedialog.askopenfilename()
     show_path['text'] = file_path
     image = Image.open(file_path)
     image.load()
-    return image
+    return file_path
+
 
     
 def display():
+    for widget in lower_frame.winfo_children():
+        widget.destroy()
     canvas = tk.Canvas(lower_frame, bg="yellow")
     canvas.pack(expand = YES, fill = BOTH)
-    img = Image.open("Emma.jpg")
+    img = Image.open(file_path)
     canvas.image = ImageTk.PhotoImage(img)
     canvas.create_image(0,0, image = canvas.image, anchor = "n")
+    
+def blacknwhite():
+    for widget in lower_frame.winfo_children():
+        widget.destroy()
+    canvas = tk.Canvas(lower_frame, bg="yellow")
+    canvas.pack(expand = YES, fill = BOTH)
+    img = Image.open(file_path)
+    bnw = img.convert('L')
+    canvas.image = ImageTk.PhotoImage(bnw)
+    canvas.create_image(0,0, image = canvas.image, anchor = "n")
 
-root = tk. Tk()
+root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
 canvas.pack()
@@ -39,10 +53,13 @@ show_path = tk.Label(frame)
 show_path.place(relwidth=0.65, relheight=1)
 
 button = tk.Button(frame, text='Wybierz zdjęcie', command = lambda: get_image())
-button.place(relx=0.7, relheight=1, relwidth=0.15)
+button.place(relx=0.7, relheight=1, relwidth=0.1)
 
 button = tk.Button(frame, text='Załaduj', command = lambda: display())
-button.place(relx=0.85, relheight=1, relwidth=0.15)
+button.place(relx=0.8, relheight=1, relwidth=0.1)
+
+button = tk.Button(frame, text="Czarno", command = lambda: blacknwhite())
+button.place(relx=0.9, relheight=1, relwidth=0.1)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
